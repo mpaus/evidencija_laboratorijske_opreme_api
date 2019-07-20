1150f093ba3e012c2a370bcd886c11ee572c1047
mpaus <matpaus@hotmail.com>
2019-07-20 14:36:37 +0200

    Removing google cloud key

--- google_cloud_storage/index.js
+++
@@ -1,9 +0,0 @@
-import { Storage } from '@google-cloud/storage';
-
-const GOOGLE_CLOUD_PROJECT_ID = 'cosmic-talent-239216'; // Replace with your project ID
-const GOOGLE_CLOUD_KEYFILE = 'evidencijaLaboratorijskeOpreme-00294fa01c04.json'; // Replace with the path to the downloaded private key
-
-export const storage = new Storage({
-    projectId: GOOGLE_CLOUD_PROJECT_ID,
-    keyFilename: GOOGLE_CLOUD_KEYFILE,
-});
